ce84b0d76a1d73caecde37b679244ae59ce88a49
James Alavosus <alavosus.james@gmail.com>
2022-06-29 02:46:41 -0400

    Improve DeferredPromise.Run() test

--- deferredpromise_test.go
+++ deferredpromise_test.go
@@ -2,8 +2,8 @@ package promise_test
 
 import (
 	"context"
-	"sync"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 
@@ -38,41 +38,67 @@ func TestDeferredPromise_ResolveAsync(t *testing.T) {
 func TestDeferredPromise_Run(t *testing.T) {
 	testCtx := context.Background()
 
+	const wantVal uint = 1
+
 	var (
-		counter uint = 0
-		wg      sync.WaitGroup
+		counter   uint = 0
+		counterCh      = make(chan uint, 2)
 	)
 
 	var fn promise.Func[uint] = func(_ context.Context, rest ...any) (uint, error) {
-		fnWg := rest[0].(*sync.WaitGroup)
-		if fnWg != nil {
-			defer fnWg.Done()
+		counter++
+
+		if len(rest) == 1 {
+			ch, ok := rest[0].(chan uint)
+			if ok {
+				ch <- counter
+			}
 		}
 
-		counter++
 		return counter, nil
 	}
 
 	prom := promise.NewDeferredPromise[uint]()
 
-	wg.Add(1)
-	prom.Run(testCtx, fn, &wg)
-	wg.Wait()
+	prom.Run(testCtx, fn, counterCh)
+	<-counterCh
 
 	assert.True(t, prom.Started())
-	assert.Equal(t, counter, uint(1))
+	assert.Equal(t, wantVal, counter)
 
 	t.Run("ensure Run() only calls fn once", func(t *testing.T) {
 		assert.True(t, prom.Started())
 
-		prom.Run(testCtx, fn, nil)
+		prom.Run(testCtx, fn, counterCh)
+
+		var (
+			counterVal = counter
+			ctxErr     error
+		)
+
+		ctx, cancel := context.WithTimeout(testCtx, 3*time.Second)
+		defer cancel()
+
+	WaitLoop:
+		for {
+			select {
+			case c := <-counterCh:
+				counterVal = c
+				break WaitLoop
+			case <-ctx.Done():
+				ctxErr = ctx.Err()
+				break WaitLoop
+			}
+		}
+
+		assert.ErrorIs(t, context.DeadlineExceeded, ctxErr)
+		assert.Equal(t, wantVal, counterVal)
+		assert.Equal(t, wantVal, counter)
 
 		res := prom.Resolve()
 		resVal := res.Result()
-
 		assert.True(t, prom.Fulfilled())
 		assert.NotNil(t, resVal)
-		assert.Equal(t, *resVal, uint(1))
-		assert.Equal(t, counter, uint(1))
+		assert.Equal(t, wantVal, *resVal)
 	})
 }
